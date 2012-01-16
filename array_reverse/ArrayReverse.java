5dc231b936c3c83898f7e922767065f0866ce7e5
Christopher Campo <ccampo.progs@gmail.com>
2012-01-16 18:17:46 -0500

    finished populate array method

--- array_reverse/ArrayReverse.java
+++ array_reverse/ArrayReverse.java
@@ -2,13 +2,31 @@
 // HW 1 - Array Reversal
 // 1/16/2012
 
+import java.util.*;
+
 public class ArrayReverse {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        int n         = 10;           // array size
+        int[] randarr = new int[n];   // array to be filled w/ random numbers
+        
+        populateArray(randarr, n);
+        
+        System.out.println(Arrays.toString(randarr));
     }
     
-    // populates array with N random numbers
-    private void populateArray(){
+    // populates array with n random numbers
+    public static void populateArray(int[] arr, int n)
+    {
+        int bound     = 100;  // generate nums between 0 and 99
+        Random numGen = new Random();
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = numGen.nextInt(bound);
+        }
+        
+        //return arr;
     }
     
     // reverses elements of given array
