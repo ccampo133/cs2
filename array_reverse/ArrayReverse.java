5366e1714e3303c19ec05cb4f77842a4e207e43b
Christopher Campo <ccampo.progs@gmail.com>
2012-01-16 18:32:25 -0500

    Finished reverseArray method

--- array_reverse/ArrayReverse.java
+++ array_reverse/ArrayReverse.java
@@ -11,29 +11,42 @@ public class ArrayReverse {
         int n         = 10;           // array size
         int[] randarr = new int[n];   // array to be filled w/ random numbers
         
-        populateArray(randarr, n);
+        populateArray(randarr);
         
         System.out.println(Arrays.toString(randarr));
+        
+        reverseArray(randarr);
+        
+        System.out.println(Arrays.toString(randarr));
+
     }
     
     // populates array with n random numbers
-    public static void populateArray(int[] arr, int n)
+    public static void populateArray(int[] arr)
     {
         int bound     = 100;  // generate nums between 0 and 99
         Random numGen = new Random();
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             arr[i] = numGen.nextInt(bound);
         }
-        
-        //return arr;
     }
     
     // reverses elements of given array
-    private void reverseArray(){
+    private static void reverseArray(int[] arr)
+    {
+        // make a copy to hold the original values
+        int[] temparr = new int[arr.length];
+        System.arraycopy(arr, 0, temparr, 0, arr.length);
+        
+        // loops from back to front to reverse the array
+        for(int i = arr.length; i > 0; i--){
+            arr[arr.length-i] = temparr[i-1];
+        }
     }
     
     // prints elements of array
-    private void printArray(){
+    private static void printArray()
+    {
     }
 }
