51663769076885df63aa501afc8678ede3cdaadd
Christopher Campo <ccampo.progs@gmail.com>
2012-01-16 20:42:23 -0500

    Tidy'd up code; using swap now instead of copying array

--- array_reverse/ArrayReverse.java
+++ array_reverse/ArrayReverse.java
@@ -5,24 +5,24 @@
 
 import java.util.*;
 
-public class ArrayReverse {
-
+public class ArrayReverse 
+{
     public static void main(String[] args)
-    {
-        int n         = 10;           // array size
-        int[] randarr = new int[n];   // array to be filled w/ random numbers
+    {    
+        int n     = 10;           // array size
+        int[] arr = new int[n];   // array to be filled w/ random numbers
         
         System.out.println("Chris Campo (ch632561): COP 3503 - Assignment 1");
-        populateArray(randarr);
         
-        System.out.print("Input: ");
-        printArray(randarr);
-
-        reverseArray(randarr);
+        populateArray(arr);
         
-        System.out.print("Output: ");
-        printArray(randarr);
+        System.out.print("Input: \t");
+        printArray(arr);
 
+        reverseArray(arr);
+        
+        System.out.print("Output: \t");
+        printArray(arr);
     }
     
     // populates array with n random numbers
@@ -39,20 +39,25 @@ public class ArrayReverse {
     // reverses elements of given array
     private static void reverseArray(int[] arr)
     {
-        // to hold the original array values, we make a copy
-        int[] temparr = new int[arr.length];
-        System.arraycopy(arr, 0, temparr, 0, arr.length);
-        
-        // loops from back to front to reverse the array
-        for(int i = arr.length; i > 0; i--){
-            arr[arr.length-i] = temparr[i-1];
+        int ibeg = 0;
+        int iend = arr.length - 1;
+        while(ibeg < iend){
+            swap(arr, ibeg, iend);
+            ibeg++;
+            iend--;
         }
+    }
+
+    private static void swap(int[] arr, int ibeg, int iend)
+    {
+        int tmp   = arr[ibeg];
+        arr[ibeg] = arr[iend];
+        arr[iend] = tmp;
     }
     
     // prints elements of array
     private static void printArray(int[] arr)
     {
         System.out.println(Arrays.toString(arr));
-    }
-    
+    }   
 }
